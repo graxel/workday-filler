d8ebbc62111203d9653a1198fffd45e8d13e97c5
graxel <grazel98@gmail.com>
2025-11-24 07:09:04 -0500

    even easier to use

--- wdf.py
+++ wdf.py
@@ -3,6 +3,7 @@ import os
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 
@@ -24,16 +25,17 @@ def refresh_on_error(max_attempts=3):
                 self.driver.refresh()
                 return wrapper(self, *args, attempts=attempts+1, **kwargs)
             except (StaleElementReferenceException) as e:
-                print(f"Attempt {attempts + 1} failed, refreshing and retrying...")
+                print(f"Attempt {attempts + 1} failed (stale element), refreshing and retrying...")
                 self.driver.refresh()
                 return wrapper(self, *args, attempts=attempts+1, **kwargs)
         return wrapper
     return decorator
 
 class FormFiller:
-    def __init__(self, driver, wait):
+    def __init__(self, driver):
         self.driver = driver
-        self.wait = wait
+        self.wait = WebDriverWait(driver, 5)
+        self.wait_longer = WebDriverWait(driver, 15)
 
     def fill_out_text_box(self, element_id, text, css_selector=False):
         if css_selector:
@@ -47,7 +49,8 @@ class FormFiller:
         input_box.send_keys(text)
     
     def select_custom_dropdown(self, button_id, options, multiselect=False, parent=0):
-        dropdown_button = self.wait.until(EC.element_to_be_clickable((By.ID, button_id)))
+        # dropdown_button = self.wait.until(EC.element_to_be_clickable((By.ID, button_id)))
+        dropdown_button = self.wait.until(EC.presence_of_element_located((By.ID, button_id)))
         self.driver.execute_script("arguments[0].scrollIntoView(true);", dropdown_button)
         self.click_button(button_id, parent=parent, css_selector=False)
 
@@ -60,7 +63,7 @@ class FormFiller:
                 # type in an option
                 dropdown_button.send_keys(option_text)
                 dropdown_button.send_keys(Keys.ENTER)
-                # # expand to see if list still open
+                # # expand code to see if list still open
                 # data-automation-id="activeListContainer"
                 
         else:
@@ -79,17 +82,23 @@ class FormFiller:
 
 
     def click_button(self, button_id, parent=0, css_selector=True):
+        time.sleep(0.2)
         if css_selector:
-            button = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, button_id)))
+            # button = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, button_id)))
+            button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, button_id)))
         else:
-            button = self.wait.until(EC.presence_of_element_located((By.ID, button_id)))
+            # button = self.wait.until(EC.presence_of_element_located((By.ID, button_id)))
+            button = self.wait.until(EC.element_to_be_clickable((By.ID, button_id)))
         for _ in range(parent):
             button = button.find_element(By.XPATH, "./..")
         self.driver.execute_script("arguments[0].scrollIntoView(true);", button)
         button.click()
 
-    def click_checkbox(self, element_id, check=True):
-        checkbox = self.wait.until(EC.presence_of_element_located((By.ID, element_id)))
+    def click_checkbox(self, element_id, check=True, css_selector=False):
+        if css_selector:
+            checkbox = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, element_id)))
+        else:
+            checkbox = self.wait.until(EC.presence_of_element_located((By.ID, element_id)))
         self.driver.execute_script("arguments[0].scrollIntoView(true);", checkbox)
         if checkbox.is_selected() != check:
             checkbox.click()
@@ -118,6 +127,11 @@ class FormFiller:
 
     def add_education(self):
         """Adds an education panel and fills it out."""
+
+        school = os.getenv("SCHOOL")
+        degree = os.getenv("DEGREE")
+        degree_backup = os.getenv("DEGREE_BACKUP")
+        major = os.getenv("MAJOR")
         # Click 'add' button to add an education panel
         self.click_button("div[aria-labelledby='Education-section'] button[data-automation-id='add-button']")
 
@@ -126,10 +140,32 @@ class FormFiller:
         panel_prefix = null_container.get_attribute("data-fkit-id").replace("--null", "")
         
         # Fill out education data
-        self.fill_out_text_box(f"{panel_prefix}--schoolName", "Rowan University")
-        self.select_custom_dropdown(f"{panel_prefix}--degree", "Bachelors")
-        self.select_custom_dropdown(f"{panel_prefix}--fieldOfStudy", "Chemical Engineering", multiselect=True, parent=2)
+        school_fields = [f"{panel_prefix}--schoolName", f"{panel_prefix}--school"]
+        for field_id in school_fields:
+            try:
+                self.select_custom_dropdown(field_id, school, multiselect=True, parent=2)
+                break
+            except:
+                pass
+        else:
+            print("Couldn't fill school name")
 
+        degrees = [degree, degree_backup]
+        for deg in degrees:
+            try:
+                self.select_custom_dropdown(f"{panel_prefix}--degree", deg)
+                break
+            except:
+                pass
+        else:
+            print("Couldn't fill degree")
+
+        try:
+            self.select_custom_dropdown(f"{panel_prefix}--fieldOfStudy", "Chemical Engineering", multiselect=True, parent=2)
+        except:
+            print("Couldn't fill field of study")
+
+            
     def upload_resume_file(self, file_path="resume.pdf"):
         file_input = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file'][data-automation-id='file-upload-input-ref']")))
         file_string = os.path.abspath(file_path)
@@ -142,11 +178,14 @@ class FormFiller:
             self.fill_out_text_box("input[data-automation-id='email']", email, css_selector=True)
             self.fill_out_text_box("input[data-automation-id='password']", password, css_selector=True)
             self.fill_out_text_box("input[data-automation-id='verifyPassword']", password, css_selector=True)
+            try:
+                self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[data-automation-id='createAccountCheckbox']")))
+                self.click_checkbox("input[data-automation-id='createAccountCheckbox']", check=True, css_selector=True)
+            except TimeoutException:
+                pass
             self.click_button("button[data-automation-id='createAccountSubmitButton']", parent=1)
-        time.sleep(0.5)
-        print("waiting...")
+        time.sleep(5)
         page_title = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h2[id='authViewTitle']")))
-        print("title found!")
         if page_title.text == "Sign In":
             self.fill_out_text_box("input[data-automation-id='email']", email, css_selector=True)
             self.fill_out_text_box("input[data-automation-id='password']", password, css_selector=True)
@@ -154,20 +193,36 @@ class FormFiller:
 
     @refresh_on_error(max_attempts=1)
     def fill_out_information_page(self):
+        country = os.getenv("COUNTRY")
+        first_name = os.getenv("FIRST_NAME")
+        last_name = os.getenv("LAST_NAME")
+        address_line1 = os.getenv("ADDRESS_LINE1")
+        city = os.getenv("CITY")
+        state_abbr = os.getenv("STATE_ABBR")
+        state_full = os.getenv("STATE_FULL")
+        postal_code = os.getenv("POSTAL_CODE")
+        phone_type = os.getenv("PHONE_TYPE")
+        phone_country_code = os.getenv("PHONE_COUNTRY_CODE")
+        phone_number = os.getenv("PHONE_NUMBER")
+
         # Select "How Did You Hear About Us?" dropdown value
-        self.select_custom_dropdown("source--source", "LinkedIn")
+        try:
+            self.wait.until(EC.presence_of_element_located((By.ID, "source--source")))
+            self.select_custom_dropdown("source--source", "LinkedIn")
+        except TimeoutException:
+            pass
         # Select radio button
         # self.click_button("input#hssh3", parent=1)  # go to label/container and click
-        self.select_custom_dropdown("country--country", "United States of America")
-        self.fill_out_text_box("name--legalName--firstName", "Kevin")
-        self.fill_out_text_box("name--legalName--lastName", "Grazel")
-        self.fill_out_text_box("address--addressLine1", "19 Delevan St")
-        self.fill_out_text_box("address--city", "Brooklyn")
-        self.select_custom_dropdown("address--countryRegion", ["NY", "New York"])  # State dropdown
-        self.fill_out_text_box("address--postalCode", "11231")
-        self.select_custom_dropdown("phoneNumber--phoneType", "Mobile")
-        self.select_custom_dropdown("phoneNumber--countryPhoneCode", "United States of America (+1)", multiselect=True, parent=2)
-        self.fill_out_text_box("phoneNumber--phoneNumber", "(856) 602-2151")
+        self.select_custom_dropdown("country--country", country)
+        self.fill_out_text_box("name--legalName--firstName", first_name)
+        self.fill_out_text_box("name--legalName--lastName", last_name)
+        self.fill_out_text_box("address--addressLine1", address_line1)
+        self.fill_out_text_box("address--city", city)
+        self.select_custom_dropdown("address--countryRegion", [state_abbr, state_full])  # State dropdown
+        self.fill_out_text_box("address--postalCode", postal_code)
+        self.select_custom_dropdown("phoneNumber--phoneType", phone_type)
+        self.select_custom_dropdown("phoneNumber--countryPhoneCode", phone_country_code, multiselect=True, parent=2)
+        self.fill_out_text_box("phoneNumber--phoneNumber", phone_number)
 
     @refresh_on_error(max_attempts=3)
     def fill_out_experience_page(self, experience_data):
